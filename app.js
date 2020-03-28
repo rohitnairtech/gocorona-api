58b73a9c9070918bc43bf69a6a017f10366fae6e
Rohit <puthenrohit@gmail.com>
2020-03-28 23:14:03 +0530

    API Built

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const PORT = process.env.PORT || 5000
 
 app.use(express.json({limit: '20mb'}));
 
@@ -10,8 +11,13 @@ const headers = {
    "x-rapidapi-key": "3bb8346e10msh2476e27ff9f9b31p15b886jsn79d66434a963"
 };
 
-let IndiaStats, WorldData;
-const WorldStats = {infected:0,dead:0,recovered:0,critical:0};
+var apiData;
+
+const updateData = ()=>{
+
+   let IndianStats, WorldData;
+   const WorldStats = {infected:0,dead:0,recovered:0,critical:0};
+
    axios.get("https://covid-193.p.rapidapi.com/statistics", {headers}).then(({data})=>{
       const {results, response} = data;
       WorldData = response;
@@ -19,7 +25,8 @@ const WorldStats = {infected:0,dead:0,recovered:0,critical:0};
       for(i = 0; i<results; i++){
          const country = response[i];
          if(country.country === 'India'){
-            IndiaStats = country;
+            const cases = country.cases;
+            IndianStats = {infected:cases.total, dead:country.deaths.total, recovered:cases.recovered, critical:cases.critical};
          }
          WorldStats.infected += country.cases.total;
          WorldStats.dead += country.deaths.total;
@@ -27,10 +34,21 @@ const WorldStats = {infected:0,dead:0,recovered:0,critical:0};
          WorldStats.critical += country.cases.critical;
       }
       console.log(WorldStats);
+   apiData = {status:200, success:true, data:{indian_stats:IndianStats, world_stats:WorldStats, world_data:WorldData}};
+   
    }).catch(e=>console.log(e));
 
+
+   setTimeout(()=>{
+      updateData();
+   },1200000);
+}
+
+updateData();
+
+
  app.get('/', (req, res)=>{
-   res.send(IndiaStats);
+   res.json(apiData);
 });
 
- app.listen(80, () => console.log(`Dashboard server is listening on ${port }`));
+ app.listen(PORT, () => console.log(`Dashboard server is listening on ${PORT}`));
