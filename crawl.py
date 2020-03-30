fcc469cec6854328246d880e107db4876fb09e87
Rohit <puthenrohit@gmail.com>
2020-03-30 10:46:24 +0530

    added comments for ease of understanding, fixed some bugs in giojs data creation

--- crawl.py
+++ crawl.py
@@ -1,25 +1,28 @@
-import spacy
+from spacy import load as sp_load
 import requests, json
 
-nlp = spacy.load("en_core_web_md")
-
-sample = ["From Mumbai To Goa", "To Wuhan", "To China"]
+nlp = sp_load("en_core_web_md")
 
 city_country_data = requests.get('https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/6ee538beca8914133259b401ba47a550313e8984/countries.min.json').json()
 country_code = requests.get('https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json').json()
 indian_cities_states = requests.get('https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json').json()
 covid_data = requests.get('https://api.rootnet.in/covid19-in/unofficial/covid19india.org/').json()['data']['rawPatientData']
-
+#Adding missing countries and their major cities
 city_country_data['Qatar'] = ['Doha', 'Abu az Zuluf', 'Abu Thaylah']
 city_country_data['Guyana'] = ['Georgetown', 'Linden', 'New Amsterdam', 'Anna Regina']
 
+#Fixing names of certain countries like, Iran, Islamic Republic to Iran
+countryFix = {'IR':'Iran', 'RU': 'Russia'}
+
 for country in country_code:
-	if country['Code'] == 'IR':
-		country['Name'] = 'Iran'
-	elif country['Code'] == 'RU':
-		country['Name'] = 'Russia' 
+	loopCount+=1
+	if country['Code'] in countryFix:
+		country['Name'] = countryFix[country['Code']]
+		del countryFix[country['Code']]
+	if not countryFix:
+		break
 
-
+#Synonyms mapping to fix issues of abbrevations
 synonyms = {'United Kingdom':['UK'],'United States':['US','USA','America'], 'United Arab Emirates':['UAE', 'Emirates', 'Abhudhabi'], 'Trinidad and Tobago':['Trinidad', 'Tobago', 'West Indies', 'Indies']}
 
 def synonymMapping(GPE):
@@ -30,8 +33,8 @@ def synonymMapping(GPE):
 				return country
 	return GPE
 
+#To check if a place is inIndia or not
 def isInIndia(GPE_name):
-	GPE_name = GPE_name.lower()
 	return next((True for item in indian_cities_states if item["name"].lower() == GPE_name or item["state"].lower() == GPE_name), False)
 
 def countryCodeMap(country):
@@ -39,6 +42,7 @@ def countryCodeMap(country):
 
 
 def countryCode(GPE_name):
+	#Normalize data
 	GPE_name = synonymMapping(GPE_name)
 	GPES = GPE_name.split(' ')
 	GPE_name = ''
@@ -51,10 +55,11 @@ def countryCode(GPE_name):
 	else:
 		GPE_name = GPES[0].capitalize()
 
-	#check if not from 
-	if not isInIndia(GPE_name):
+	#check if not from India
+	GPE_lower = GPE_name.lower()
+	if not isInIndia(GPE_lower):
 		if GPE_name in city_country_data:
-			return countryCodeMap(GPE_name.lower())
+			return countryCodeMap(GPE_lower)
 		else:
 			for country in city_country_data:
 				if GPE_name in city_country_data[country]:
@@ -101,12 +106,12 @@ def addGioData(e, i='IN'):
 				gio["v"] += 1
 				break
 			if gioDataLen == (x+1):
-				giojs.append({'e':e, 'i':'IN', 'v':1})
+				giojs.append({'e':e, 'i':i, 'v':1})
 	else:
 		print('EEEEE')
 		print(e)
 		print(i)
-		giojs.append({'e':e, 'i':'IN', 'v':1})	
+		giojs.append({'e':e, 'i':i, 'v':1})	
 
 
 
@@ -124,12 +129,12 @@ for country in fromCountry:
 				print(x)
 				print(GPE_count-1)
 				print(x == (GPE_count-1))
-				addGioData(code, country[x+1][0])				
+				addGioData(code, country[x+1])				
 	else:
 		addGioData(country[0])
 
 
-with open('data.json', 'w') as outfile:
+with open('patient_id_internation_travel.json', 'w') as outfile:
     json.dump(fromCountry, outfile)
 
 with open('gio.json', 'w') as outfile:
